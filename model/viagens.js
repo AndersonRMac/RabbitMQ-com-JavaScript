110d9fcd6858084fdca9897eb8247fa6eadd7ac9
anderson.maciel <anderson.maciel@vanguardatech.com>
2022-09-09 14:05:16 -0300

    craição do model viagem

--- model/viagens.js
+++ model/viagens.js
@@ -1,6 +1,13 @@
+//classe viagem
 
-module.exports = class viagens{
+module.exports = class Viagens{
+    constructor(VEI_ID, EQP_SRN){
+        this._VEI_ID = VEI_ID;
+        this._EQP_SRN = EQP_SRN;
 
-   
+    } 
 }
+
+var viagens = new Viagens(111, 222);
+console.log(viagens._EQP_SRN);
 
